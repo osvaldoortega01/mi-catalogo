83559a82a829413bdd49eeaad302bd280b198dae
Osvaldo Ortega <osvaldo.ortega01@gmail.com>
2021-10-28 21:19:00 -0500

    E2 A4

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AUTOMOVILES } from '../data';
 import { Automovil } from '../models';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
+
 export class ListComponent implements OnInit {
   closeResult = '';
   autos: Automovil[] = [];
   autoSeleccionado? : Automovil;
-
-  constructor() { }
+  
 
   ngOnInit(): void {
     this.autos = AUTOMOVILES;
   }
 
+  onSelect(auto : Automovil, content: any){
+    this.autoSeleccionado = auto;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 }
