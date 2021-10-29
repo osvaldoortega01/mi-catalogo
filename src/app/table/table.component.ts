99d51b1d86edcf2b14319c2e6412ba5a3a162d54
Osvaldo Ortega <osvaldo.ortega01@gmail.com>
2021-10-28 22:26:00 -0500

    E3 A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AUTOMOVILES } from '../data';
+import { Automovil } from '../models';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
 
+  closeResult = '';
+  autos: Automovil[] = [];
+  autoSeleccionado? : Automovil;
   constructor() { }
 
   ngOnInit(): void {
+    this.autos = AUTOMOVILES;
   }
-
 }
